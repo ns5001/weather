570b54cda3190fde7f894063a1d95db252230294
Naveed Shah <naveed@tayybah.com>
2017-03-04 18:45:12 -0500

    seccond commit - building out functionality on weather.js

--- weather.js
+++ weather.js
@@ -1,16 +1,25 @@
 const https = require('https');
 const api = require('./api.json');
 
+function printWeather(weather) {
+  const message = `Current temperature in ${weather.location.city} is ${weather.current_observation.temp_f}F`;
+  console.log(message);
+}
+
 function get(query) {
-  const request =
-  https.get('https://api.wunderground.com/api/${api.key}/conditions/q/${query}.json',
-  response => {
+  const request = https.get('https://api.wunderground.com/api/${api.key}/conditions/q/${query}.json', response => {
+   if (response.statusCode == 200)
     let body = "";
     response.on('data', chunk => {
       body += chunk;
     });
+
     response.on('end', () => {
-      console.log(body);
+      try{
+        const weather = JSON.parse(body);
+
+
+      }
     });
   });
 }
